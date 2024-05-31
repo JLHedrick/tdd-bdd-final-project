8976165d62c98c70e62203a5810460b15405163f
JLHedrick <158624884+JLHedrick@users.noreply.github.com>
2024-05-31 16:16:29 -0700

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -101,7 +101,14 @@ def create_products():
 #
 # PLACE YOUR CODE TO LIST ALL PRODUCTS HERE
 #
-
+ def test_get_product(self):
+        """It should Get a single Product"""
+        # get the id of a product
+        test_product = self._create_products(1)[0]
+        response = self.client.get(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data["name"], test_product.name)
 ######################################################################
 # R E A D   A   P R O D U C T
 ######################################################################
