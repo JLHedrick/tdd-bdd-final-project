ccd8402df4b82eff95bc9f6660c978f4179ef367
JLHedrick <158624884+JLHedrick@users.noreply.github.com>
2024-05-31 17:51:28 -0700

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -203,6 +203,25 @@ def update_products(product_id):
     product.update()
     return product.serialize(), status.HTTP_200_OK
 
+
+  ######################################################################
+# DELETE A PRODUCT
+######################################################################
+@app.route("/products/<int:product_id>", methods=["DELETE"])
+def delete_products(product_id):
+    """
+    Delete a Product
+
+    This endpoint will delete a Product based the id specified in the path
+    """
+    app.logger.info("Request to Delete a product with id [%s]", product_id)
+
+    product = Product.find(product_id)
+    if product:
+        product.delete()
+
+    return "", status.HTTP_204_NO_CONTENT
+
   
     ######################################################################
     # Utility functions
